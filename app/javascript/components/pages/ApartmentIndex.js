8cdc87f4d9070f12b32dcaae2f026cc01847c520
lundrew <andrueee@gmail.com>
2022-05-18 09:21:29 -0700

    index

--- app/javascript/components/pages/ApartmentIndex.js
+++ app/javascript/components/pages/ApartmentIndex.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react'
 export default class ApartmentIndex extends Component {
   render() {
     return (
-      <h3>ApartmentIndex</h3>
+      <>
+      <h3>Available Apartments</h3>
+      <li>Bonita Villas</li>
+      <li>Hanover Little Italy</li>
+      <li>Mira Bella</li>
+      <li>Broadstone Little Italy</li>
+      <li>Metro Mission Valley</li>
+      </>
     )
   }
 }
