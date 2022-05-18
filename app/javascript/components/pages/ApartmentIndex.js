3c32d1c0924a9ad9721204f3a7faf803027c1b8f
lundrew <andrueee@gmail.com>
2022-05-18 09:35:23 -0700

    indent/ul tags

--- app/javascript/components/pages/ApartmentIndex.js
+++ app/javascript/components/pages/ApartmentIndex.js
@@ -5,11 +5,13 @@ export default class ApartmentIndex extends Component {
     return (
       <>
       <h3>Available Apartments</h3>
-      <li>Bonita Villas</li>
-      <li>Hanover Little Italy</li>
-      <li>Mira Bella</li>
-      <li>Broadstone Little Italy</li>
-      <li>Metro Mission Valley</li>
+        <ul>
+          <li>Bonita Villas</li>
+          <li>Hanover Little Italy</li>
+          <li>Mira Bella</li>
+          <li>Broadstone Little Italy</li>
+          <li>Metro Mission Valley</li>
+        </ul>
       </>
     )
   }
