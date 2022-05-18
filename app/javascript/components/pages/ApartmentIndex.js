26f39c816b88688af44eaf972ad41946c1b9488d
lundrew <andrueee@gmail.com>
2022-05-18 16:41:38 -0700

    removed app. show

--- app/javascript/components/pages/ApartmentIndex.js
+++ app/javascript/components/pages/ApartmentIndex.js
@@ -8,24 +8,6 @@ export default class ApartmentIndex extends Component {
     return (
       <>
       <h3>Available Apartments</h3>
-      <br />
-      <br />
-      <Col sm="6">
-          {this.props.apartments && this.props.apartments.map(apartment => {
-            return(
-              <Card body key={apartment.street}>
-                <CardImg alt='apartment image' src= {apartment.image}/>
-                <br />
-                <CardTitle>
-                  <h3>{apartment.street}</h3>
-                  <h4>{apartment.price}</h4>
-                </CardTitle>
-                
- 
-              </Card>
-            )
-          })}
-        </Col>
       </>
     )
   }
